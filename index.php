af344c6918ece6420eeed9a47c65f86c30b9fcad
sam chaudry <sam.chaudry@hotmail.co.uk>
2017-03-21 23:21:23 +0000

    count update

--- index.php
+++ index.php
@@ -37,29 +37,52 @@ if($_SERVER["REQUEST_METHOD"] == "POST") {
 
 ?>
 
-<!DOCTYPE html>
-<html lang="en">
+
+<html>
+
 <head>
-    <meta charset="UTF-8">
-    <title>Title</title>
+    <title>Login Page</title>
+
+    <style type = "text/css">
+        body {
+            font-family:Arial, Helvetica, sans-serif;
+            font-size:14px;
+        }
+
+        label {
+            font-weight:bold;
+            width:100px;
+            font-size:14px;
+        }
+
+        .box {
+            border:#666666 solid 1px;
+        }
+    </style>
+
 </head>
-<body>
-<h1>It's Alive!!!</h1>
 
-<form action="" method="post">
-    <div class="imgcontainer">
+<body bgcolor = "#FFFFFF">
+
+<div align = "center">
+    <div style = "width:300px; border: solid 1px #333333; " align = "left">
+        <div style = "background-color:#333333; color:#FFFFFF; padding:3px;"><b>Login</b></div>
+
+        <div style = "margin:30px">
+
+            <form action = "" method = "post">
+                <label>UserName  :</label><input type = "text" name = "username" class = "box"/><br /><br />
+                <label>Password  :</label><input type = "password" name = "password" class = "box" /><br/><br />
+                <input type = "submit" value = " Submit "/><br />
+            </form>
+
+            <div style = "font-size:11px; color:#cc0000; margin-top:10px"><?php echo $error; ?></div>
+
+        </div>
+
     </div>
 
-    <div class="container">
-        <label><b>Username</b></label>
-        <input type="text" placeholder="Enter Username" name="username" required>
-
-        <label><b>Password</b></label>
-        <input type="password" placeholder="Enter Password" name="password" required>
-
-        <button type="submit">Login</button>
-    </div>
-</form>
+</div>
 
 </body>
 </html>
