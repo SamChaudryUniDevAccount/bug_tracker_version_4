cb2606e2430a892a0e3c2d599d2aee301168e944
sam chaudry <sam.chaudry@hotmail.co.uk>
2017-03-21 23:19:58 +0000

    count update

--- index.php
+++ index.php
@@ -7,27 +7,33 @@ session_start();
 
 if($_SERVER["REQUEST_METHOD"] == "POST") {
 
+    // username and password sent from form
+
     $myusername = mysqli_real_escape_string($db,$_POST['username']);
     $mypassword = mysqli_real_escape_string($db,$_POST['password']);
 
-    $sql = "SELECT username , password FROM user WHERE username = '$myusername' and passcode = '$mypassword'";
+    $sql = "SELECT id FROM admin WHERE username = '$myusername' and passcode = '$mypassword'";
     $result = mysqli_query($db,$sql);
     $row = mysqli_fetch_array($result,MYSQLI_ASSOC);
     $active = $row['active'];
 
     $count = mysqli_num_rows($result);
 
-    if($count = mysqli_num_rows($result) == 1) {
+    echo $count;
+
+    if($count == 1) {
+
+        session_register("myusername");
 
         $_SESSION['login_user'] = $myusername;
 
         header("location: landing.php");
-
     }else {
-
-        echo  "Your Login Name or Password is invalid";
+        $error = "Your Login Name or Password is invalid";
     }
 }
+
+
 
 ?>
 
