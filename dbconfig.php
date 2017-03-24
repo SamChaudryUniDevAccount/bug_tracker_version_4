e21a0d9eb2ca6a8ee9e7dda17b3ca8fe53356fdf
sam chaudry <sam.chaudry@hotmail.co.uk>
2017-03-24 11:39:31 +0000

    Update connection string

--- dbconfig.php
+++ dbconfig.php
@@ -1,20 +1,13 @@
 <?php
 
-//$connectstr_dbhost = 'http://sc-cmm503.azurewebsites.net/';
-//$connectstr_dbname = 'localdb';
-//$connectstr_dbusername = 'username';
-//$connectstr_dbpassword = 'password';
 
 $connectstr_dbhost = '';
 $connectstr_dbname = '';
 $connectstr_dbusername = '';
 $connectstr_dbpassword = '';
 
-
 foreach ($_SERVER as $key => $value) {
-
     if (strpos($key, "MYSQLCONNSTR_localdb") !== 0) {
-
         continue;
     }
 
@@ -24,7 +17,7 @@ foreach ($_SERVER as $key => $value) {
     $connectstr_dbpassword = preg_replace("/^.*Password=(.+?)$/", "\\1", $value);
 }
 
-$db = mysqli_connect($connectstr_dbhost, $connectstr_dbusername, $connectstr_dbpassword,$connectstr_dbname);
+$link = mysqli_connect($connectstr_dbhost, $connectstr_dbusername, $connectstr_dbpassword,$connectstr_dbname);
 
 if (!$link) {
     echo "Error: Unable to connect to MySQL." . PHP_EOL;
@@ -35,3 +28,43 @@ if (!$link) {
 
 echo "Success: A proper connection to MySQL was made! The my_db database is great." . PHP_EOL;
 echo "Host information: " . mysqli_get_host_info($link) . PHP_EOL;
+
+
+
+
+//
+////$connectstr_dbhost = 'http://sc-cmm503.azurewebsites.net/';
+////$connectstr_dbname = 'localdb';
+////$connectstr_dbusername = 'username';
+////$connectstr_dbpassword = 'password';
+//
+//$connectstr_dbhost = '';
+//$connectstr_dbname = '';
+//$connectstr_dbusername = '';
+//$connectstr_dbpassword = '';
+//
+//
+//foreach ($_SERVER as $key => $value) {
+//
+//    if (strpos($key, "MYSQLCONNSTR_localdb") !== 0) {
+//
+//        continue;
+//    }
+//
+//    $connectstr_dbhost = preg_replace("/^.*Data Source=(.+?);.*$/", "\\1", $value);
+//    $connectstr_dbname = preg_replace("/^.*Database=(.+?);.*$/", "\\1", $value);
+//    $connectstr_dbusername = preg_replace("/^.*User Id=(.+?);.*$/", "\\1", $value);
+//    $connectstr_dbpassword = preg_replace("/^.*Password=(.+?)$/", "\\1", $value);
+//}
+//
+//$db = mysqli_connect($connectstr_dbhost, $connectstr_dbusername, $connectstr_dbpassword,$connectstr_dbname);
+//
+//if (!$link) {
+//    echo "Error: Unable to connect to MySQL." . PHP_EOL;
+//    echo "Debugging errno: " . mysqli_connect_errno() . PHP_EOL;
+//    echo "Debugging error: " . mysqli_connect_error() . PHP_EOL;
+//    exit;
+//}
+//
+//echo "Success: A proper connection to MySQL was made! The my_db database is great." . PHP_EOL;
+//echo "Host information: " . mysqli_get_host_info($link) . PHP_EOL;
