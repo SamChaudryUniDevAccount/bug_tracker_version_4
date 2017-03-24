dd0a366f54352b2e7e7f684e901032c8ae13141c
sam chaudry <sam.chaudry@hotmail.co.uk>
2017-03-24 12:01:49 +0000

    fixes2

--- index.php
+++ index.php
@@ -1,5 +1,9 @@
 <?php
 
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
 
 include("dbconfig.php");
 
@@ -9,16 +13,16 @@ if($_SERVER["REQUEST_METHOD"] == "POST") {
 
     // username and password sent from form
 
-    echo "unsecured username is {$_POST['username']} and password is {$_POST['password']}";
+//    echo "unsecured username is {$_POST['username']} and password is {$_POST['password']}";
 
 
     $myusername = mysqli_real_escape_string($link,$_POST['username']);
     $mypassword = mysqli_real_escape_string($link,$_POST['password']);
 
-    echo "username is {$myusername} and password is {$mypassword}";
+ //   echo "username is {$myusername} and password is {$mypassword}";
 
     $sql = "SELECT username , password FROM user WHERE username = '$myusername' and passcode = '$mypassword'";
-    $result = mysqli_query($db,$sql);
+    $result = mysqli_query($link,$sql);
     $row = mysqli_fetch_array($result,MYSQLI_ASSOC);
     $active = $row['active'];
 
