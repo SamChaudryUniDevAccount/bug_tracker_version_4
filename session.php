7c1418745580ddf088a3ce9488f193ed9d52cfdd
sam chaudry <sam.chaudry@hotmail.co.uk>
2017-03-26 17:46:47 +0100

    session manager text commented out

--- session.php
+++ session.php
@@ -13,9 +13,9 @@ $user_check = $_SESSION['login_user'];
 
 $ses_sql = mysqli_query($link,"select username from admin where username = '$user_check' ");
 
-$row = mysqli_fetch_array($ses_sql,MYSQLI_ASSOC);
+//$row = mysqli_fetch_array($ses_sql,MYSQLI_ASSOC);
 
-$login_session = $row['username'];
+//$login_session = $row['username'];
 
 if(!isset($_SESSION['login_user'])){
 
